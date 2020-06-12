613675d4b075cdd2350a79b79aecd8c0e605340c
MaoranSun <31110005+MaoranSun@users.noreply.github.com>
2020-06-11 20:10:26 -0400

    Add files via upload

---
+++ projects/project3/sketch.js
@@ -0,0 +1,28 @@
+let x = 30;
+let y = 30;
+let r = 50;
+let direction = 6;
+let elt = document.getElementById('canvas');
+
+
+function setup() {
+  let cnv = createCanvas(900, 600);
+  cnv.parent(elt)
+}
+
+function draw() {
+  background(51);
+  push()
+  noStroke();
+  fill('red');
+  circle(x, y, r);
+  pop();
+  if(x<900-r/2 && x>r/2) {
+    x += direction;
+  }
+  else if(x >= 900 - r/2 || x <= r/2) {
+    direction = direction * -1;
+    x += direction;
+  }
+
+}
